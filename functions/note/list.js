4dd943a54ef5b750535fd5aee70bd3fba0b313d2
Mauricio <mauricio@outlook.com>
2020-05-18 18:29:17 -0300

    adding sorting by createdDate desc

--- functions/note/list.js
+++ functions/note/list.js
@@ -16,10 +16,19 @@ export async function main(event, context) {
     }
   };
 
+  /**
+   * Compare in descending order of Date
+   * @param {Date} item1
+   * @param {Date} item2
+   */
+  function compareCreatedAt(item1, item2) {
+    return item2.createdAt - item1.createdAt;
+  }
+
   try {
     const result = await dynamodb.call("query", params);
     // Return the matching list of items in response body
-    return success(result.Items);
+    return success(result.Items.sort(compareCreatedAt));
   } catch (e) {
     return failure({ status: false });
   }
